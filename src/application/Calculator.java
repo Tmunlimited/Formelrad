1ac59031c6d07bf6ec7c213c835641c19b3d5ac6
josuakoglin <josua.koglin@bluewin.ch>
2018-11-22 10:15:02 +0100

    calculate R from U and I

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -2,85 +2,94 @@ package application;
 
 /**
  * Berechnet das Formelrad
+ *
  * @author Peter Rutschmann
  * @version 13.09.2018
  */
 public class Calculator {
-	private double leistung;
-	private double spannung;
-	private double strom;
-	private double widerstand;
-	
-	public Calculator(double leistung, double spannung, double strom, double widerstand) {
-		super();
-		this.leistung = leistung;
-		this.spannung = spannung;
-		this.strom = strom;
-		this.widerstand = widerstand;
-	}
-	
-	public double getLeistung() {
-		return leistung;
-	}
-	
-	public double getSpannung() {
-		return spannung;
-	}
+    private double leistung;
+    private double spannung;
+    private double strom;
+    private double widerstand;
 
-	public double getStrom() {
-		return strom;
-	}
+    public Calculator(double leistung, double spannung, double strom, double widerstand) {
+        super();
+        this.leistung = leistung;
+        this.spannung = spannung;
+        this.strom = strom;
+        this.widerstand = widerstand;
+    }
 
-	public double getWiderstand() {
-		return widerstand;
-	}
+    public double getLeistung() {
+        return leistung;
+    }
 
-	@Override
-	public String toString() {
-		return "Calculator [leistung=" + leistung + 
-				", spannung=" + spannung + 
-				", strom=" + strom + 
-				", widerstand="	+ widerstand + "]";
-	}
+    public double getSpannung() {
+        return spannung;
+    }
 
-	public void calculate() {
-		/* Hier auf Grund der vorhanden Werte entscheiden
-		 * welche Methode unten aufgerufen werden muss.
-		 */
-	}
-	
-	/* Hier die Methoden mit den Formlen hinzuf�gen
-	 */
+    public double getStrom() {
+        return strom;
+    }
+
+    public double getWiderstand() {
+        return widerstand;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculator [leistung=" + leistung +
+                ", spannung=" + spannung +
+                ", strom=" + strom +
+                ", widerstand=" + widerstand + "]";
+    }
+
+    public void calculate() {
+        /* Hier auf Grund der vorhanden Werte entscheiden
+         * welche Methode unten aufgerufen werden muss.
+         */
 
 
-	public double calculateUFromRAndI(float r, float i) {
-		return r*i;
-	}
+    }
 
-	public double calculateUFromPAndI(float p, float i) {
-		return p/i;
-	}
+        /* Hier die Methoden mit den Formlen hinzuf�gen
+         */
+
+    //calculate U
+    public double calculateUFromRAndI(double r, double i) {
+        return r * i;
+    }
+
+    public double calculateUFromPAndI(double p, double i) {
+        return p / i;
+    }
+
+    public double calculateUFromPAndR(double p, double r) {
+        return Math.sqrt(p * r);
+    }
 
 
-	public double calculateUFromPAndR(float p, float r) {
-		return Math.sqrt(p*r);
-	}
+    //calculate R
+    public double calculateRFromUAndI(double u, double i) {
+        return u / i;
+    }
+    
 
-	
-	// Leistungsformeln f�r P Start
-	
-	public double getPfromUandI(int u, int i) {
-		return u*i;
-	}
-	
-	public double getPfromRandI(int r, int i) {
-		return r*i*i;
-	}
-	
-	public double getPfromUandR(int u, int r) {
-		return (u*u)/r;
-	}
-	
-	// Leistungsformlen f�r P Ende
+    // Leistungsformeln f�r P Start
+
+    public double getPfromUandI(double u, double i) {
+        return u * i;
+    }
+
+    public double getPfromRandI(double r, double i) {
+        return r * Math.pow(2, i);
+    }
+
+    public double getPfromUandR(double u, double r) {
+        return (u * u) / r;
+    }
+
+    // Leistungsformlen f�r P Ende
+
 
 }
